71a7fb7ed452a35d486ae588c65fdfead36b8e35
Otavio F. G. Pane <otavio.pane@gmail.com>
2021-10-05 17:00:38 -0300

    Atualizando erro 404

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,4 @@
-from django.http import Http404
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Question
 
