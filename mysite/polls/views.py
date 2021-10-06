aa22a8e86ed71b03d0513bdb8324a48bba0faadf
Otavio F. G. Pane <otavio.pane@gmail.com>
2021-10-06 14:51:27 -0300

    Criando versao real da implementaçao da funçao results()

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -12,11 +12,11 @@ def index(request):
 
 
 def results(request, question_id):
-    response = "You're looking at the results os question %s"
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
-def vote(request, question_id):
+def vote(request, question': question):
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
